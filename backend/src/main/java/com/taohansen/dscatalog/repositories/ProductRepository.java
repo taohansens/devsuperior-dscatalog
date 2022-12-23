3bdaed826a5ee21772bc316d52c6cecf127f91ff
taohansens <taohansens@gmail.com>
2022-12-22 23:06:54 -0300

    Product repository, service, resource. GET ok

---
+++ backend/src/main/java/com/taohansen/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.taohansen.dscatalog.repositories;
+
+import com.taohansen.dscatalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
